c9c27ce98fcf8184e6db8d1f191807610d96b014
Janagale <mmmwcr@gmail.com>
2018-09-30 12:52:12 +0300

    add crossorigin

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbycqwLnLUcWN4W0CWkFSogURvdv6jTwSv0FouDKYzMJfP9qpIa4/exec';
+    url = 'https://crossorigin.me/https://script.google.com/macros/s/AKfycbycqwLnLUcWN4W0CWkFSogURvdv6jTwSv0FouDKYzMJfP9qpIa4/exec';
 
 $('#submit-form').on('click', function(e) {
   e.preventDefault();
