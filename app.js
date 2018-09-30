18dab6a24a9650654d8c432402b7d45c39d49fdd
Janagale <mmmwcr@gmail.com>
2018-09-30 14:01:56 +0300

    add error report

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var $form = $('form#test-form'),
-    url = 'https://crossorigin.me/https://script.google.com/macros/s/AKfycbycqwLnLUcWN4W0CWkFSogURvdv6jTwSv0FouDKYzMJfP9qpIa4/exec';
+  url = 'https://crossorigin.me/https://script.google.com/macros/s/AKfycbycqwLnLUcWN4W0CWkFSogURvdv6jTwSv0FouDKYzMJfP9qpIa4/exec';
 
-$('#submit-form').on('click', function(e) {
+$('#submit-form').on('click', function (e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
     method: "GET",
     dataType: "json",
     data: $form.serialize(),
-      success: function(success){
-    console.log("Data Saved.")
-}
+    success: function (success) {
+      console.log("Data Saved.")
+    },
+    error: function (e) {
+      console.log('Error Occured. ' + e);
+    }
   })
 
 })
